09ec0dd706d4f8675ae96b5f814637c428117233
tamittu <tamittu@naver.com>
2024-05-11 00:33:26 +0900

    Activate mobile menu button

--- src/components/header/InnerHeader.tsx
+++ src/components/header/InnerHeader.tsx
@@ -1,11 +1,15 @@
+import Link from "next/link";
 import React from "react";
 
 const InnerHeader = () => {
   return (
     <div className="p-2 border-y border-light-gray block md:hidden bg-white dark:bg-black">
-      <span className="px-4 sm:px-8 text-sm font-base dark:text-white">
-        감정일기 쓰기
-      </span>
+      <Link href="/emotions">
+        <span className="px-4 sm:px-8 text-sm font-base dark:text-white">
+          감정일기 쓰기
+        </span>
+      </Link>
+
       <span className="text-sm font-base dark:text-white">감정 분석</span>
     </div>
   );
