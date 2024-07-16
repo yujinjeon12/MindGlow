144b78c619033014705f89895b401eb52b4d0106
yujinjeon12 <tamittu@naver.com>
2024-07-17 01:49:29 +0900

    refactor: extract handler logic into CanvasHandler component #8

--- src/components/canvas/Canvas.tsx
+++ src/components/canvas/Canvas.tsx
@@ -1,44 +1,28 @@
 "use client";
-import React, { useState, useEffect, useRef } from "react";
-import { BsEraserFill } from "react-icons/bs";
-import { IoMdReturnLeft, IoMdReturnRight } from "react-icons/io";
-import Button from "../button/Button";
-import NextImage from "next/image";
+import React, { useEffect, useRef } from "react";
 import { debounce } from "@/utils/utils";
-import { ImBin } from "react-icons/im";
-import ModalBase from "../modal/ModalBase";
 import { useSelector, useDispatch } from "react-redux";
 import { Path } from "@/lib/features/canvas/CanvasSlice";
 import {
   startDrawing,
   draw,
   stopDrawing,
-  setLineWidth,
-  setLineAlpha,
-  setColor,
-  setEraseMode,
-  clearPaths,
-  undo,
-  redo,
   setModalOpen
 } from "@/lib/features/canvas/CanvasSlice";
 import { RootState } from "@/lib/store";
+import CanvasHandler from "./CanvasHandler";
 
 const Canvas = () => {
   const dispatch = useDispatch();
   const canvasRef = useRef<HTMLCanvasElement>(null);
-  const colorRef = useRef<HTMLInputElement>(null);
   const {
     paths,
-    undonePaths,
     lineWidth,
     lineAlpha,
     color,
     isDrawing,
     eraseMode,
-    modalIsOpen,
   } = useSelector((state: RootState) => state.CanvasReducer);
-  const [ctx, setCtx] = useState<CanvasRenderingContext2D | null>(null);
 
   useEffect(() => {
     const canvas = canvasRef.current; // canvas element
@@ -65,7 +49,6 @@ const Canvas = () => {
     const context = canvas.getContext("2d");
     if (!context) return;
 
-    setCtx(context);
     const wrapper = canvas.parentNode;
     if (wrapper instanceof Element) {
       // css 크기 설정
@@ -167,40 +150,8 @@ const Canvas = () => {
       ctx.stroke();
     });
   };
-  const erase = () => {
-    dispatch(setEraseMode(true));
-  };
-  const eraseAll = () => {
-    const canvas = canvasRef.current;
-    const ctx = canvas?.getContext("2d");
-    if (canvas && ctx) {
-      dispatch(clearPaths()); // Clear the paths and undone paths
-      clearCanvas();
-    }
-  };
-  const handleSave = () => {
-    const link = document.createElement("a");
-    link.download = "draw-emotion.png";
-    link.href = canvasRef.current?.toDataURL() ?? "";
-    link.click();
-  };
-
-  const handleLineWidth = (e: React.MouseEvent) => {
-    e.stopPropagation();
-    dispatch(setModalOpen(true));
-  };
   const handleOverlay = () => {
     dispatch(setModalOpen(false));
-  };
-  const handleUndo = () => {
-    if (paths.length > 0) {
-      dispatch(undo());
-    }
-  };
-  const handleRedo = () => {
-    if (undonePaths.length) {
-      dispatch(redo());
-    }
   };
 
   const touchStart = (e: React.TouchEvent<HTMLCanvasElement>) => {
@@ -275,96 +226,7 @@ const Canvas = () => {
             draw emotion canvas
           </canvas>
         </div>
-        <div className="flex h-fit relative items-center z-0 bottom-16 bg-light-gray p-2 rounded-md">
-          {modalIsOpen && (
-            <ModalBase customStyles="absolute -top-14 left-10 p-4">
-              <div className="flex flex-col items-start">
-                <span className="text-lg font-bold">선 두께 {lineWidth}</span>
-                <input
-                  type="range"
-                  min="1"
-                  max="50"
-                  defaultValue={lineWidth}
-                  onChange={(e) => {
-                    if (ctx) {
-                      dispatch(setLineWidth(Number(e.target.value)));
-                    }
-                  }}
-                />
-                <span className="text-lg font-bold">선 투명도 {lineAlpha}</span>
-                <input
-                  type="range"
-                  min="1"
-                  max="10"
-                  defaultValue={lineAlpha}
-                  onChange={(e) => {
-                    if (ctx) {
-                      dispatch(setLineAlpha(Number(e.target.value)));
-                    }
-                  }}
-                />
-              </div>
-            </ModalBase>
-          )}
-          <input
-            ref={colorRef}
-            type="color"
-            width="0"
-            height="0"
-            value="#fefefe"
-            className={`inline-block w-0 invisible caret-transparent`}
-            onChange={(e) => {
-              dispatch(setColor(e.target.value));
-            }}
-          ></input>
-          <span
-            className={`rounded-md w-6 h-6 inline-block align-middle`}
-            style={{ backgroundColor: color }}
-            onMouseDown={(e) => {
-              e.preventDefault();
-              colorRef.current?.click();
-              dispatch(setEraseMode(false));
-            }}
-            onTouchStart={(e) => {
-              e.preventDefault();
-              colorRef.current?.click();
-              dispatch(setEraseMode(false));
-            }}
-          ></span>
-          <NextImage
-            className="mx-4 inline-block w-7 h-7 cursor-pointer align-middle"
-            src="/images/linewidth.svg"
-            width={28}
-            height={28}
-            alt="line-width"
-            onClick={handleLineWidth}
-          />
-          <BsEraserFill
-            className="w-6 h-6 inline-block cursor-pointer align-middle mr-4 text-dark-gray"
-            onClick={erase}
-          />
-          <IoMdReturnLeft
-            className="w-6 h-6 inline-block cursor-pointer align-middle mr-4 bg-gray rounded-full p-1"
-            color={paths.length == 0 ? "white" : "black"}
-            onClick={handleUndo}
-          />
-          <IoMdReturnRight
-            className="w-6 h-6 inline-block cursor-pointer align-middle mr-4 bg-gray rounded-full p-1"
-            color={undonePaths.length == 0 ? "white" : "black"}
-            onClick={handleRedo}
-          />
-          <ImBin
-            className="w-6 h-6 inline-block cursor-pointer align-middle mr-4 text-dark-gray"
-            onClick={eraseAll}
-          />
-          <Button
-            onClick={handleSave}
-            bgColor="bg-pink"
-            textColor="text-white"
-            value="SAVE"
-            option="w-14 h-8 rounded-md text-sm"
-          />
-        </div>
+        <CanvasHandler canvasRef={canvasRef}></CanvasHandler>
       </div>
     </section>
   );
